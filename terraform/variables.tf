696290862147d5cecf7a35d397061f8afcdf97f9
samarareddy <samara.junna@gmail.com>
2025-05-16 11:43:08 +0530

    backend

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "project_name" {
+    default = "expense"
+  
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        project = "expense"
+        environment = "dev"
+        terraform = "true"
+        Component = "backend"
+        # appVersion = "v1.0.0"
+    }
+}
+
+# variable "zone_name" {
+#     default = "mydaws.online"
+# }
+variable "domain_name" {
+    default = "mydaws.fun"
+  
+}
+
+variable "app_Version" {
+  
+}
