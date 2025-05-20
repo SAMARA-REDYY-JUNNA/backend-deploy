bb7e26f77ec77cc673e43fb7ea1ce3505e08f68d
samarareddy <samara.junna@gmail.com>
2025-05-20 10:33:20 +0530

    dev

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -2,6 +2,6 @@
 component=$1
 environment=$2
 app_Version=$3
-dnf install ansible -y
+apt install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/SAMARA-REDYY-JUNNA/expense-ansible-roles1-tf.git main.yaml -e component=$backend -e env=$environment -e appVersion=$app_Version
+ansible-pull -i localhost, -U https://github.com/SAMARA-REDYY-JUNNA/expense-ansible-roles1-tf.git backend.yaml -e component=backend  -e appVersion=$app_Version
